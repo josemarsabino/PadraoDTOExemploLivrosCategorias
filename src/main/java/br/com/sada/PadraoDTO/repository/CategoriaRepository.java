f689606f4b48c81b49dc6762e55099439d706bd1
josemarsabino <josemarsabino@hotmail.com>
2022-01-09 12:06:43 -0300

    Configuração Repository

--- src/main/java/br/com/sada/PadraoDTO/repository/CategoriaRepository.java
+++ src/main/java/br/com/sada/PadraoDTO/repository/CategoriaRepository.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Repository;
 
 import br.com.sada.PadraoDTO.model.Categoria;
 
-
+@Repository
 public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
 
 }
--- src/main/java/br/com/sada/PadraoDTO/repository/LivroRepository.java
+++ src/main/java/br/com/sada/PadraoDTO/repository/LivroRepository.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Repository;
 
 import br.com.sada.PadraoDTO.model.Livro;
 
-
+@Repository
 public interface LivroRepository extends JpaRepository<Livro, Long> {
 
 }
