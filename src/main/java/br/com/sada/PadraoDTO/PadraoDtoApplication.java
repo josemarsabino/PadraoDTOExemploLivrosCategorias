0f6331c3a6da31e13121cae55a5f382246c81938
josemarsabino <josemarsabino@hotmail.com>
2022-01-09 11:04:10 -0300

    Projeto Inicial Sprint Initializer

---
+++ src/main/java/br/com/sada/PadraoDTO/PadraoDtoApplication.java
@@ -0,0 +1,13 @@
+package br.com.sada.PadraoDTO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PadraoDtoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PadraoDtoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/sada/PadraoDTO/PadraoDtoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sada.PadraoDTO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PadraoDtoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
